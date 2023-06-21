68dc01caa866bf0344a4153d32c142a59fc1b20d
yohan <sjohn9227@naver.com>
2023-06-21 17:48:55 +0900

    feat: room repository 생성

---
+++ src/main/java/com/example/MnM/boundedContext/chat/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.example.MnM.boundedContext.chat.repository;
+
+import com.example.MnM.boundedContext.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<ChatRoom, Long> {
+
+}
