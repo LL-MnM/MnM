850c68c05dc8c0f2d039a3a73ebce52f333e11ef
nagiltae <nagt1997@gmail.com>
2023-06-21 15:18:34 +0900

    feat : add security dependency in gradle, security config, custom userdetail service

--- src/main/java/com/example/MnM/boundedContext/member/entity/Member.java
+++ src/main/java/com/example/MnM/boundedContext/member/entity/Member.java
@@ -10,8 +10,13 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -44,4 +49,15 @@ public class Member {
     private String profileImage; //프로필사진, 임시로 만듬
     private String introduce; //자기소개
 
+    public List<? extends GrantedAuthority> getGrantedAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        grantedAuthorities.add(new SimpleGrantedAuthority("member"));
+
+        if ("admin".equals(username)) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
+        }
+
+        return grantedAuthorities;
+    }
 }
