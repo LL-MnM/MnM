c79cf7b89ed09abb60352124b3603a71e269ef57
yohan <sjohn9227@naver.com>
2023-06-22 17:41:30 +0900

    feat: 필요 어노테이션 추가
    async, auditing, cache, schedule 추가

--- src/main/java/com/example/MnM/MnMApplication.java
+++ src/main/java/com/example/MnM/MnMApplication.java
@@ -2,7 +2,15 @@ package com.example.MnM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableJpaAuditing
+@EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class MnMApplication {
 
