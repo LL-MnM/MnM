42e9398a1f65264dd15fe705d09817fb8420222a
yohan <sjohn9227@naver.com>
2023-06-22 17:39:19 +0900

    feat: message 1:1  n:m 분리

--- src/main/java/com/example/MnM/base/config/WebSocketConfig.java
+++ src/main/java/com/example/MnM/base/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/sub","/single");
         registry.setApplicationDestinationPrefixes("/pub");
     }
 
