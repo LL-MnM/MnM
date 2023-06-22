a9a56c619dae595c3bd69c70b0a961f285136f09
Choi-Hyeok99 <gur0709@naver.com>
2023-06-22 14:35:38 +0900

    Answer 저장 , AnswerRepository 내용 추가

--- src/main/java/com/example/MnM/boundedContext/board/answer/AnswerRepository.java
+++ src/main/java/com/example/MnM/boundedContext/board/answer/AnswerRepository.java
@@ -2,7 +2,14 @@ package com.example.MnM.boundedContext.board.answer;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface AnswerRepository extends JpaRepository<Answer , Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "ALTER TABLE answer AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
 }
---
+++ src/main/java/com/example/MnM/boundedContext/board/answer/AnswerService.java
@@ -0,0 +1,25 @@
+package com.example.MnM.boundedContext.board.answer;
+
+
+import com.example.MnM.boundedContext.board.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+
+    public void create(Question question, String content){
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        answerRepository.save(answer);
+    }
+
+}
