ea323fe3c5545acba13a5234b03bbed7873812cc
yohan <sjohn9227@naver.com>
2023-06-23 11:06:50 +0900

    feat: test yml 추가 및 room delete 메소드 추가

--- src/main/java/com/example/MnM/boundedContext/chat/service/RoomService.java
+++ src/main/java/com/example/MnM/boundedContext/chat/service/RoomService.java
@@ -5,15 +5,17 @@ import com.example.MnM.boundedContext.chat.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class RoomService {
 
-    private final RedisTemplate<String,Object> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
     private final RoomRepository roomRepository;
 
 
@@ -21,7 +23,7 @@ public class RoomService {
         return roomRepository.findAll();
     }
 
-    public Long createRoom( ) {
+    public Long createRoom() {
         String roomSecretId = UUID.randomUUID().toString();
 
         ChatRoom room = roomRepository.save(ChatRoom.builder()
@@ -30,8 +32,16 @@ public class RoomService {
                 .name(roomSecretId)
                 .build());
 
-        return room.getId();
+        Long roomId = room.getId();
+        redisTemplate.opsForList().rightPush("rooms", roomId);
 
+        return roomId;
+
+    }
+
+    public void deleteRoom(String roomId) {
+        roomRepository.deleteByUniqueId(roomId);
+        redisTemplate.opsForList().remove("rooms",1,roomId);
     }
 
     public ChatRoom findById(Long id) {
