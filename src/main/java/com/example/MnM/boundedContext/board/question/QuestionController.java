d5e1d65744aac91b2792cedd9b5c9c75c60fd82e
Choi-Hyeok99 <gur0709@naver.com>
2023-06-23 09:43:41 +0900

    QuestionForm 생성

--- src/main/java/com/example/MnM/boundedContext/board/question/QuestionController.java
+++ src/main/java/com/example/MnM/boundedContext/board/question/QuestionController.java
@@ -36,7 +36,11 @@ public class QuestionController {
     }
 
     @PostMapping("question/create")
-    public String questionCreate(String subject, String content) {
+    public String questionCreate(QuestionForm questionForm) {
+
+        String subject = questionForm.getSubject();
+        String content = questionForm.getContent();
+
         if (subject == null || subject.trim().length() == 0){
             throw new RuntimeException("subject(을)를 입력해주세요");
         }
