a6b0708c728cf26c82407fad1076f5a9d9895cd2
Choi-Hyeok99 <gur0709@naver.com>
2023-06-23 09:43:41 +0900

    QuestionForm 생성

---
+++ src/main/java/com/example/MnM/boundedContext/board/question/QuestionForm.java
@@ -0,0 +1,14 @@
+package com.example.MnM.boundedContext.board.question;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class QuestionForm {
+
+    private String subject;
+
+    private String content;
+
+}
