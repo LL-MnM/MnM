226f2489e03f25f13886f45fecfb58a9889cacdd
Choi-Hyeok99 <gur0709@naver.com>
2023-06-23 16:17:49 +0900

    Feat:페이징 리스트 , 페이징 역순 구현

--- src/main/java/com/example/MnM/boundedContext/board/question/QuestionRepository.java
+++ src/main/java/com/example/MnM/boundedContext/board/question/QuestionRepository.java
@@ -1,4 +1,6 @@
 package com.example.MnM.boundedContext.board.question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
@@ -9,6 +11,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
+
 
 
 
