53c7bc3f095a8a47fd49e64f9de8fd99571ab3f4
Choi-Hyeok99 <gur0709@naver.com>
2023-06-23 11:33:46 +0900

    Feat: 답변등록 경고 기능 구현

--- src/main/java/com/example/MnM/boundedContext/board/answer/AnswerController.java
+++ src/main/java/com/example/MnM/boundedContext/board/answer/AnswerController.java
@@ -1,16 +1,16 @@
 package com.example.MnM.boundedContext.board.answer;
 
 
-import ch.qos.logback.core.joran.util.beans.BeanDescriptionFactory;
 import com.example.MnM.boundedContext.board.question.Question;
 import com.example.MnM.boundedContext.board.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/answer")
@@ -20,11 +20,20 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable Integer id, @RequestParam String content) {
+    public String createAnswer(
+            Model model,
+            @PathVariable Integer id,
+            @Valid AnswerForm answerForm,
+            BindingResult bindingResult
+    ) {
 
         Question question = questionService.getQuestion(id);
 
-        answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "board/question_detail";
+        }
+        Answer answer = answerService.create(question, answerForm.getContent());
 
         return "redirect:/question/detail/%d".formatted(id);
     }
