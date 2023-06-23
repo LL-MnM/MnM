5f2c8ded93b019f1103cfc99d33facfd3e0d1bb0
Choi-Hyeok99 <gur0709@naver.com>
2023-06-23 10:38:19 +0900

    Feat: 게시물 작성 내용이 비면 경고

--- src/main/java/com/example/MnM/boundedContext/board/question/QuestionForm.java
+++ src/main/java/com/example/MnM/boundedContext/board/question/QuestionForm.java
@@ -9,12 +9,12 @@ import lombok.Setter;
 @Getter
 public class QuestionForm {
 
-    @NotBlank
-    @Size(max = 100)
+    @NotBlank(message = "제목은 필수항목입니다.")
+    @Size(max = 100 , message = "제목을 100자 이하로 입력해주세요.")
     private String subject;
 
-    @NotBlank
-    @Size(max = 200)
+    @NotBlank(message = "내용은 필수항목입니다.")
+    @Size(max = 200, message = "내용을 200자 이하로 입력해주세요.")
     private String content;
 
 }
