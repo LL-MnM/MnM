66981ca63ca928afe14b306880eed1e466900978
Choi-Hyeok99 <gur0709@naver.com>
2023-06-26 16:51:53 +0900

    refactor: 테스트 코드 수정2

--- src/test/java/com/example/MnM/Board/BoardApplicationTests.java
+++ src/test/java/com/example/MnM/Board/BoardApplicationTests.java
@@ -7,6 +7,8 @@ import com.example.MnM.boundedContext.board.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class BoardApplicationTests {
 
     @Autowired
