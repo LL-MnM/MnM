fa92316fd95a373b9fe6fd2433839d66b7e1a976
wnstn7671 <wnstn7671@naver.com>
2023-06-26 10:22:39 +0900

    fix: Merge Error/3

--- src/main/java/com/example/MnM/base/initData/NotProd.java
+++ src/main/java/com/example/MnM/base/initData/NotProd.java
@@ -22,17 +22,6 @@ public class NotProd {
             @Transactional
             public void run(String... args) throws Exception {
 
-                Member member1 = memberService.join(new MemberController.JoinForm(
-                        "user1", "1234", "user1@email.com", "홍길동", "홍길동", 170, 20, "서울", "남자", "ENFP",
-                        "운동", "안녕하세요", ""
-                )).getData();
-
-                Member member2 = memberService.join(new MemberController.JoinForm(
-                        "user3", "1234", "user2@email.com", "임꺽정", "임꺽정", 170, 20, "서울", "남자", "ENFP",
-                        "운동", "안녕하세요", ""
-                )).getData();
-
-                //임의로 회원 생성
             }
         };
     }
