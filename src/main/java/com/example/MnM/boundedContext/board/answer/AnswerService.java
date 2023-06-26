341d550ab11155ed9de88cb73ebae7f3514194c1
Choi-Hyeok99 <gur0709@naver.com>
2023-06-26 15:36:45 +0900

    refactor : @Transactional 설정

--- src/main/java/com/example/MnM/boundedContext/board/answer/AnswerService.java
+++ src/main/java/com/example/MnM/boundedContext/board/answer/AnswerService.java
@@ -4,9 +4,11 @@ package com.example.MnM.boundedContext.board.answer;
 import com.example.MnM.boundedContext.board.question.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class AnswerService {
