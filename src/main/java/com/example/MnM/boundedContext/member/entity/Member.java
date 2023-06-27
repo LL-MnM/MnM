362bc26efa245e1f74130f326b0994f4510b0aeb
nagiltae <nagt1997@gmail.com>
2023-06-27 18:01:04 +0900

    feat : add NomatchUserLevelException

--- src/main/java/com/example/MnM/boundedContext/member/entity/Member.java
+++ src/main/java/com/example/MnM/boundedContext/member/entity/Member.java
@@ -2,6 +2,7 @@ package com.example.MnM.boundedContext.member.entity;
 
 import com.example.MnM.base.baseEntity.BaseEntity;
 import com.example.MnM.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.example.MnM.boundedContext.member.entity.emum.UserLevel;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Builder;
@@ -30,16 +31,20 @@ public class Member extends BaseEntity {
     private String username; //id
     private String password; //pw
     private String name; //이름
-    private String nickname; //닉네임
+
     @Email
     private String email; //이메일
     private Boolean emailVerified; //이메일 인증 확인
     private String providerType; //소셜로그인을 위한 제공자 타입
 
+    @Convert(converter = UserLevel.class)
+    private UserLevel UserLevel;
 
+    private boolean deleted = Boolean.FALSE; //soft delete
 
     //여기까지 회원가입시 기본정보, 아래로는 개인정보
 
+    private String nickname; //닉네임
     private Integer height; //키
     private Integer age; //나이;
     private String locate; //지역
@@ -49,7 +54,6 @@ public class Member extends BaseEntity {
     private String profileImage; //프로필사진, 임시로 만듬
     private String introduce; //자기소개
 
-    private boolean deleted = Boolean.FALSE; //soft delete
 
     public Member(String userId, String username, String password) {
         this.username = userId;
