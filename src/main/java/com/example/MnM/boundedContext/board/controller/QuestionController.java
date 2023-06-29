aef3148d6298569312a371d71f54b19f76036f53
Choi-Hyeok99 <gur0709@naver.com>
2023-06-29 09:28:32 +0900

    Feat: 질문 삭제기능 구현

--- src/main/java/com/example/MnM/boundedContext/board/controller/QuestionController.java
+++ src/main/java/com/example/MnM/boundedContext/board/controller/QuestionController.java
@@ -90,5 +90,18 @@ public class QuestionController {
         this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("question/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+
+        if (!question.getMember().getUserId().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+
+        questionService.delete(question);
+
+        return "redirect:/";
+    }
 }
 
