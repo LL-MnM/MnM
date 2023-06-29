df2f1759c7f99e0805e0364a4ed7a93683d816bf
hjkim2040 <hjkim2661@gmail.com>
2023-06-29 23:11:46 +0900

    Design: 메인페이지 로고 추가 & 소개 페이지 구현

--- src/main/java/com/example/MnM/boundedContext/mbtiTest/controller/MbtiTestController.java
+++ src/main/java/com/example/MnM/boundedContext/mbtiTest/controller/MbtiTestController.java
@@ -1,9 +1,11 @@
 package com.example.MnM.boundedContext.mbtiTest.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@PreAuthorize("isAuthenticated()")
 public class MbtiTestController {
 
 	@GetMapping("/mbtiTest")
