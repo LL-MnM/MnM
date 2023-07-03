680b00c8d68ce2e7fcc134f1132c47ccde9d3f60
Choi-Hyeok99 <gur0709@naver.com>
2023-07-03 15:44:42 +0900

    Feat: 좋아요 기능 구현

--- src/main/java/com/example/MnM/boundedContext/board/entity/question/Question.java
+++ src/main/java/com/example/MnM/boundedContext/board/entity/question/Question.java
@@ -6,10 +6,12 @@ import com.example.MnM.boundedContext.board.entity.answer.Answer;
 import com.example.MnM.boundedContext.member.entity.Member;
 import jakarta.persistence.*;
 
+
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+
 import lombok.ToString;
-
-
 import lombok.Getter;
 
 @Getter
@@ -29,15 +31,22 @@ public class Question extends BaseEntity {
     @JoinColumn(name = "member_nickname")
     private Member member;
 
+    @ManyToMany
+    private Set<Member> userId = new LinkedHashSet<>();
+
     public void createQuestion(String subject, String content, Member member) {
         this.subject = subject;
         this.content = content;
         this.member = member;
-
     }
+
     public void updateQuestion(String subject, String content) {
         this.subject = subject;
         this.content = content;
     }
 
+    public void addVoter(Member voter)
+    {
+        this.userId.add(voter);
+    }
 }
