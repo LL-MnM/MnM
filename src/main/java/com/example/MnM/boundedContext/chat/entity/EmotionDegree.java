d107f10656ba60005edc7b1f7ee291e71c46bf00
yohan <sjohn9227@naver.com>
2023-07-03 21:48:17 +0900

    feat: 구글 로직 인증 로직 수정

--- src/main/java/com/example/MnM/boundedContext/chat/entity/EmotionDegree.java
+++ src/main/java/com/example/MnM/boundedContext/chat/entity/EmotionDegree.java
@@ -5,7 +5,7 @@ import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(exclude = {"magnitude","score"})
 @Getter
 @Embeddable
 public class EmotionDegree {
