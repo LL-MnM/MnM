c670bddfce78b66762c88445e6e4ccb7028c6385
yohan <sjohn9227@naver.com>
2023-07-04 17:58:00 +0900

    feat: 추천 로직 개선 및 테스트 코드 작성

--- src/main/java/com/example/MnM/boundedContext/recommend/service/MemberMbtiService.java
+++ src/main/java/com/example/MnM/boundedContext/recommend/service/MemberMbtiService.java
@@ -1,5 +1,6 @@
 package com.example.MnM.boundedContext.recommend.service;
 
+import com.example.MnM.boundedContext.chat.service.EmotionService;
 import com.example.MnM.boundedContext.member.entity.Member;
 import com.example.MnM.boundedContext.member.service.MemberService;
 import com.example.MnM.boundedContext.recommend.entity.Mbti;
@@ -16,11 +17,18 @@ public class MemberMbtiService {
 
     private final MbtiService mbtiService;
     private final MemberService memberService;
+    private final EmotionService emotionService;
 
     public List<Member> findMbtiForMember(Member member) {
+
+        if (emotionService.isExistsEmotionDegree(member.getId())) {
+            String mbtiName = emotionService.findBestMbtiByDegree(member.getId());
+            return memberService.findByMbti(mbtiName);
+        }
+
         Mbti mbti = mbtiService.findByName(member.getMbti());
-        String name = mbti.getBestMbti();
-        List<Member> members = memberService.findByMbti(name);
+        List<Member> members = memberService.findByMbti(mbti.getBestMbti());
         return members;
+
     }
 }
