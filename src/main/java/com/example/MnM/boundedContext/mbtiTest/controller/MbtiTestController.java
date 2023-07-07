56a9905f5d7dbf0cd62030b92b7e0c4e335afccb
hjkim2040 <hjkim2661@gmail.com>
2023-07-07 17:03:18 +0900

    Design: question-form UI 수정

--- src/main/java/com/example/MnM/boundedContext/mbtiTest/controller/MbtiTestController.java
+++ src/main/java/com/example/MnM/boundedContext/mbtiTest/controller/MbtiTestController.java
@@ -1,11 +1,9 @@
 package com.example.MnM.boundedContext.mbtiTest.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@PreAuthorize("isAuthenticated()")
 public class MbtiTestController {
 
 	@GetMapping("/mbtiTest")
