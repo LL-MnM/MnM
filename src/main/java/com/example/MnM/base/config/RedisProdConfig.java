6fb4460e59e285ae2f3c859eb43b3d8de090e1d0
yohan <sjohn9227@naver.com>
2023-07-09 14:26:31 +0900

    feat: redis cluster 도입

---
+++ src/main/java/com/example/MnM/base/config/RedisProdConfig.java
@@ -0,0 +1,76 @@
+package com.example.MnM.base.config;
+
+import com.example.MnM.base.redis.RedisInfo;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Profile({"prod"})
+@RequiredArgsConstructor
+@EnableRedisRepositories
+@Configuration
+public class RedisProdConfig {
+
+    @Value("${spring.data.redis.cluster.nodes}")
+    List<String> nodes;
+
+    @Value("${spring.data.redis.database}")
+    private int database;
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        redisTemplate.setEnableTransactionSupport(true);
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(nodes);
+
+        ClusterTopologyRefreshOptions clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .enableAllAdaptiveRefreshTriggers()
+                .enablePeriodicRefresh(Duration.ofHours(1L))
+                .build();
+        ClientOptions clientOptions = ClusterClientOptions.builder()
+                .topologyRefreshOptions(clusterTopologyRefreshOptions)
+                .build();
+
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.of(10, ChronoUnit.SECONDS))
+                .clientOptions(clientOptions)
+                .build();
+
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisClusterConfiguration, clientConfiguration);
+        connectionFactory.setDatabase(database);
+
+        return connectionFactory;
+    }
+}
