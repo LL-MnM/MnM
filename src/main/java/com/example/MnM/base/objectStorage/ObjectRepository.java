f3685d21d9a48b0a22579765e3c4f4087941072d
yohan <sjohn9227@naver.com>
2023-07-10 17:11:44 +0900

    feat: 불필요한 S3 테스트 코드 삭제

--- src/main/java/com/example/MnM/base/objectStorage/ObjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MnM.base.objectStorage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ObjectRepository extends JpaRepository<ObjectFile, Long> {
-    ObjectFile findByUrl(String url);
-}
