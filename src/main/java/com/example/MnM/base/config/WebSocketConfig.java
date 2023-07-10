aa4296150a3d140cae561da4d3d1a07b92a8d1d1
yohan <sjohn9227@naver.com>
2023-07-10 17:10:08 +0900

    feat: cors 허용

--- src/main/java/com/example/MnM/base/config/WebSocketConfig.java
+++ src/main/java/com/example/MnM/base/config/WebSocketConfig.java
@@ -30,6 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat/connect")
                 .setAllowedOrigins(AppConfig.getChatUrl())
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
