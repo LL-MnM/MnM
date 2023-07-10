05f8603bb60de1fa3aeb0fab4ffb12fbdecd5d0b
yohan <sjohn9227@naver.com>
2023-07-10 11:34:22 +0900

    feat: 도커 파일 수정

--- src/main/java/com/example/MnM/base/objectStorage/config/NaverObjectConfigure.java
+++ src/main/java/com/example/MnM/base/objectStorage/config/NaverObjectConfigure.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile({"naver","test","prod"})
+@Profile({"dev","test","prod"})
 @Configuration
 public class NaverObjectConfigure{
     @Value("${cloud.aws.credentials.access-key}")
