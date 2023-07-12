dfebf84fcb93063739277aeea42e480a2395a598
nagiltae <nagt1997@gmail.com>
2023-07-12 14:46:04 +0900

    fix : delete member url fix, csrf token move to head

--- src/test/java/com/example/MnM/boundedContext/member/controller/MemberControllerTest.java
+++ src/test/java/com/example/MnM/boundedContext/member/controller/MemberControllerTest.java
@@ -24,12 +24,13 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 
 
 @SpringBootTest
@@ -48,13 +49,52 @@ class MemberControllerTest {
     Member member;
 
     @BeforeEach
-    public void Test() {
+    public void Test() { //테스트 멤버 작성
         member = Member.builder()
                 .username("test001")
                 .name("홍길동")
                 .password("1234")
                 .build();//testcase
     }
+/*회원가입*/
+    @DisplayName("회원가입 폼")
+    void showMemberJoinTest() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/join"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showJoin"))
+                .andExpect(content().string(containsString("회원가입")));
+    }
+
+        @Test
+        @DisplayName("회원가입")
+        void MemberJoinTest() throws Exception {
+            // WHEN
+            ResultActions resultActions = mvc
+                    .perform(post("/member/join")
+                            .with(csrf())
+                            .param("username", "test001")
+                            .param("password", "1234")
+                            .param("email", "user1@test.com")
+                    )
+                    .andDo(print());
+
+            // THEN
+            resultActions
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(handler().handlerType(MemberController.class))
+                    .andExpect(handler().methodName("join"))
+                    .andExpect(redirectedUrlPattern("/member/login?msg=**"));
+
+            assertThat(memberService.findByUsername("user10").isPresent()).isTrue();
+        }
+
 
     @Test
     @WithUserDetails(value = "user1")
@@ -72,30 +112,8 @@ class MemberControllerTest {
 
     @Test
     @WithUserDetails(value = "user1")
-    @DisplayName("회원 수정")
-    void memberModify() throws Exception {
-
-        ResultActions resultActions = mvc
-                .perform(post("/member/editMyPage")
-                        .with(csrf())
-                        .param("nickname", "유저1닉네임")
-                        .param("email", "user1@google.com")
-                ).andDo(print());
-
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("editMyPage"))
-                .andExpect(status().is3xxRedirection());
-
-        Optional<Member> findMember = memberService.findByUserName("user1");
-        assertThat(findMember.get().getEmail()).isEqualTo("user1@google.com");
-        assertThat(findMember.get().getNickname()).isEqualTo("유저1닉네임");
-    }
-
-    @Test
-    @WithUserDetails(value = "user1")
     @DisplayName("회원 탈퇴")
-    void t003() throws Exception {
+    void deleteMember() throws Exception {
         ResultActions resultActions = mvc
                 .perform(get("/member/delete")
                 ).andDo(print());
@@ -122,6 +140,125 @@ class MemberControllerTest {
         assertThat(afterDelete).isNotEmpty();
         assertThat(afterDelete.get().isDeleted()).isTrue();
     }
+
+
+    @Test
+    @WithUserDetails(value = "user1")
+    @DisplayName("회원 수정 폼")
+    void showMemberModifyTest() throws Exception {
+
+        ResultActions resultActions = mvc
+                .perform(post("/member/editMyPage")
+                        .with(csrf())
+                        .param("nickname", "유저1닉네임")
+                        .param("email", "user1@google.com")
+                ).andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("editMyPage"))
+                .andExpect(status().is3xxRedirection());
+
+        Optional<Member> findMember = memberService.findByUserName("user1");
+        assertThat(findMember.get().getEmail()).isEqualTo("user1@google.com");
+        assertThat(findMember.get().getNickname()).isEqualTo("유저1닉네임");
+    }
+
+    @Test
+    @WithUserDetails(value = "user1")
+    @DisplayName("회원 수정")
+    void memberModifyTest() throws Exception {
+
+        ResultActions resultActions = mvc
+                .perform(post("/member/editMyPage")
+                        .with(csrf())
+                        .param("nickname", "유저1닉네임")
+                        .param("email", "user1@google.com")
+                ).andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("editMyPage"))
+                .andExpect(status().is3xxRedirection());
+
+        Optional<Member> findMember = memberService.findByUserName("user1");
+        assertThat(findMember.get().getEmail()).isEqualTo("user1@google.com");
+        assertThat(findMember.get().getNickname()).isEqualTo("유저1닉네임");
+    }
+
+
+    /*
+    @Test
+    @DisplayName("아이디찾기 폼")
+    void t3() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/findUsername"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showFindUsername"))
+                .andExpect(content().string(containsString("아이디 찾기")));
+    }
+
+    @Test
+    @DisplayName("아이디찾기")
+    void t4() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/member/findUsername")
+                        .with(csrf())
+                        .param("email", "user1@test.com")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is3xxRedirection())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("findUsername"))
+                .andExpect(redirectedUrlPattern("/member/login?**"));
+    }
+
+    @Test
+    @DisplayName("비밀번호 찾기 폼")
+    void t5() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/findPassword"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showFindPassword"))
+                .andExpect(content().string(containsString("비밀번호 찾기")));
+    }
+
+    @Test
+    @DisplayName("비밀번호 찾기")
+    void t6() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/member/findPassword")
+                        .with(csrf())
+                        .param("email", "user1@test.com")
+                        .param("username", "user1")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is3xxRedirection())
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("findPassword"))
+                .andExpect(redirectedUrlPattern("/member/login?**"));
+    }*/
+
 
 }
 
